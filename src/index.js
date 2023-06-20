fe7f132cb2de5e4de478e9ecd714017efead197e
Roshan_Padal <roshanpadal91@gmail.com>
2023-06-20 18:05:13 +0530

    Fixed linting issue

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 // make bluebird default Promise
-Promise = require("bluebird"); // eslint-disable-line no-global-assign
-const { port, env } = require("./config/vars");
-const logger = require("./config/logger");
-const app = require("./config/express");
-const mongoose = require("./config/mongoose");
+Promise = require('bluebird'); // eslint-disable-line no-global-assign
+const { port, env } = require('./config/vars');
+const logger = require('./config/logger');
+const app = require('./config/express');
+const mongoose = require('./config/mongoose');
 
 // open mongoose connection
 mongoose.connect();
